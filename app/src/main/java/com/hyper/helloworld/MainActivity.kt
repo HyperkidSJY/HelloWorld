4929fa5abe02110032779577428602c5bbbff79e
SHIVAM <shivamyadav9735@gmail.com>
2022-12-20 19:36:42 +0530

    click to change text color

--- app/src/main/java/com/hyper/helloworld/MainActivity.kt
+++ app/src/main/java/com/hyper/helloworld/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.hyper.helloworld
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +17,9 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener{
             Log.v("hello world","button clicked")
             Toast.makeText(this,"hello to you too!", Toast.LENGTH_SHORT).show()
+            val textView = findViewById<TextView>(R.id.introTextView)
+            textView.setTextColor(Color.parseColor("#FF0000"))
         }
+
     }
 }
